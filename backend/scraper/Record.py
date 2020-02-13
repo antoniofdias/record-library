4a2ed2a33bc38633bedfba7e1432a37b93457916
Tom <antoniofdiass@gmail.com>
2020-02-13 18:20:43 -0300

    Add cover art to records

--- backend/scraper/Record.py
+++ backend/scraper/Record.py
@@ -1,5 +1,6 @@
 class Record:
-    def __init__(self, artist, title, price):
+    def __init__(self, artist, title, price, cover):
         self.artist = artist
         self.title = title
         self.price = price
+        self.cover = cover
--- backend/scraper/main.py
+++ backend/scraper/main.py
@@ -3,6 +3,7 @@ from Record import Record
 from utils import soupify, checkIfVinyl
 
 my_url = 'https://www.sensorialdiscos.com.br/search_store?q='
+main_url = 'https://www.sensorialdiscos.com.br'
 
 args = sys.argv
 args.pop(0)
@@ -17,13 +18,14 @@ containers = page_soup.findAll("li", {"class": "product"})
 records = []
 
 for container in containers:
-    disc_url = 'https://www.sensorialdiscos.com.br' + container.a["href"]
+    disc_url = main_url + container.a["href"]
     disc_soup = soupify(disc_url)
 
     if checkIfVinyl(disc_soup):
         name = disc_soup.find("div", {"id": "product_title"}).text
         parsed_name = name.split(" - ", 1)
         price = disc_soup.find("span", {"class": "price"}).text
+        cover = main_url + disc_soup.find("img", {"alt": name})["src"]
 
-        new_record = Record(parsed_name[0], parsed_name[1], price)
+        new_record = Record(parsed_name[0], parsed_name[1], price, cover)
         records.append(new_record)
